53e626f75b3e22e2b2a4a67063a938643563c7af
Florian Aßmann <fassmann@fork.de>
2009-08-06 18:38:52 +0200

    ApplicationHelper#icon now accepts states.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
-  def icon(name)
+  def icon(name, state = :default)
+    %Q'<span class=".ui-state-#{ state }">%s</span>' %
     %Q'<span class="ui-icon ui-icon-#{ name }"></span>'
   end
 
